347d6f6a7ef16787f0a1744ec065df6303c3c3a6
chris.ng93 <chris.ng93@gmail.com>
2016-07-01 19:07:04 -0700

    complete some error handling for fitbit data

--- server/lib/fitbitDatabaseHelpers.js
+++ server/lib/fitbitDatabaseHelpers.js
@@ -5,18 +5,22 @@ const conversions = require('./conversions');
 
 module.exports = {
   syncIterateThrough: (data, userid, type, array, insert, cb) => {
-    const syncTasks = [];
-    data[array].forEach((datum) => {
-      syncTasks.push((cb) => {
-        insert(datum, userid, type, cb);
+    if (!data.errors) {
+      const syncTasks = [];
+      data[array].forEach((datum) => {
+        syncTasks.push((cb) => {
+          insert(datum, userid, type, cb);
+        });
       });
-    });
-    syncTasks.push((callback) => {
-      callback();
-    });
-    const callback = () => console.log('Inserted', type, 'items into database');
-    utils.syncMap(syncTasks, callback, []);
-    cb();
+      syncTasks.push((callback) => {
+        callback();
+      });
+      const callback = () => console.log('Inserted', type, 'items into database');
+      utils.syncMap(syncTasks, callback, []);
+      cb();
+    } else {
+      cb();
+    }
   },
 
   insertIntoDatabase: (datum, userid, type, cb) => {
